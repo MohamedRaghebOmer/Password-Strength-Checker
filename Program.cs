0b5b09ce60c8ba6680dad9b3342af1db9f8354cc
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2025-12-21 22:10:50 +0200

    Create program UI, labels, textbox, ...

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PasswordStrengthChecker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmMainForm());
         }
     }
 }
