627241d197898470fd7e188d9ead3b8d873087fb
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2025-12-22 19:07:00 +0200

    Create fun's \'IsCommonPassword\', \'HasCharacterAppearedMoreThanThreeTimes\', add notes at form1.cs [Design], and set maximum password length to 64 char

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,15 +35,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.cbShowPassword = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lblTimeToCrack = new System.Windows.Forms.Label();
+            this.lblCharContaining = new System.Windows.Forms.Label();
             this.lblLowerCase = new System.Windows.Forms.Label();
             this.lblUperCase = new System.Windows.Forms.Label();
             this.lblNumbers = new System.Windows.Forms.Label();
             this.lblSymbols = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.lblOverlay = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.lblStrength = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtPassword
@@ -52,6 +55,7 @@
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPassword.ForeColor = System.Drawing.Color.Black;
             this.txtPassword.Location = new System.Drawing.Point(588, 379);
+            this.txtPassword.MaxLength = 64;
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '•';
             this.txtPassword.Size = new System.Drawing.Size(528, 45);
@@ -66,11 +70,11 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(297, 615);
+            this.label1.Location = new System.Drawing.Point(350, 636);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(848, 38);
+            this.label1.Size = new System.Drawing.Size(776, 38);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Tip: Avoid the use of dictionary words or common names,";
+            this.label1.Text = "Avoid the use of dictionary words or common names";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -79,7 +83,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(412, 659);
+            this.label2.Location = new System.Drawing.Point(379, 674);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(619, 38);
             this.label2.TabIndex = 2;
@@ -120,27 +124,27 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Time to crack your password:";
             // 
-            // label5
+            // lblTimeToCrack
             // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(797, 516);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(126, 30);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "0 Seconds";
+            this.lblTimeToCrack.AutoSize = true;
+            this.lblTimeToCrack.BackColor = System.Drawing.Color.Transparent;
+            this.lblTimeToCrack.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeToCrack.Location = new System.Drawing.Point(797, 516);
+            this.lblTimeToCrack.Name = "lblTimeToCrack";
+            this.lblTimeToCrack.Size = new System.Drawing.Size(126, 30);
+            this.lblTimeToCrack.TabIndex = 6;
+            this.lblTimeToCrack.Text = "0 Seconds";
             // 
-            // label6
+            // lblCharContaining
             // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(583, 431);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(223, 25);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "0 Character Containing:";
+            this.lblCharContaining.AutoSize = true;
+            this.lblCharContaining.BackColor = System.Drawing.Color.Transparent;
+            this.lblCharContaining.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCharContaining.Location = new System.Drawing.Point(583, 431);
+            this.lblCharContaining.Name = "lblCharContaining";
+            this.lblCharContaining.Size = new System.Drawing.Size(223, 25);
+            this.lblCharContaining.TabIndex = 7;
+            this.lblCharContaining.Text = "0 Character Containing:";
             // 
             // lblLowerCase
             // 
@@ -195,7 +199,7 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(366, 755);
+            this.label7.Location = new System.Drawing.Point(366, 779);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(672, 22);
             this.label7.TabIndex = 12;
@@ -214,17 +218,53 @@
             this.lblOverlay.Text = "Enter Password";
             this.lblOverlay.Click += new System.EventHandler(this.lblOverlay_Click);
             // 
-            // label8
+            // lblStrength
             // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(710, 232);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(321, 60);
-            this.label8.TabIndex = 14;
-            this.label8.Text = "No Password";
+            this.lblStrength.AutoSize = true;
+            this.lblStrength.BackColor = System.Drawing.Color.Transparent;
+            this.lblStrength.Font = new System.Drawing.Font("Microsoft YaHei", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStrength.ForeColor = System.Drawing.Color.White;
+            this.lblStrength.Location = new System.Drawing.Point(707, 173);
+            this.lblStrength.Name = "lblStrength";
+            this.lblStrength.Size = new System.Drawing.Size(321, 60);
+            this.lblStrength.TabIndex = 14;
+            this.lblStrength.Text = "No Password";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei", 15.2F);
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(634, 304);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(561, 35);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Maximum password length is 64 characters.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei", 16.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Yellow;
+            this.label10.Location = new System.Drawing.Point(565, 301);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 38);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Note:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Yellow;
+            this.label11.Location = new System.Drawing.Point(288, 633);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(88, 45);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Tip: ";
             // 
             // frmMainForm
             // 
@@ -234,15 +274,18 @@
             this.BackgroundImage = global::PasswordStrengthChecker.Properties.Resources.BackgroundImage;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1347, 806);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblStrength);
             this.Controls.Add(this.lblOverlay);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.lblSymbols);
             this.Controls.Add(this.lblNumbers);
             this.Controls.Add(this.lblUperCase);
             this.Controls.Add(this.lblLowerCase);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblCharContaining);
+            this.Controls.Add(this.lblTimeToCrack);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cbShowPassword);
             this.Controls.Add(this.label3);
@@ -269,15 +312,18 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckBox cbShowPassword;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTimeToCrack;
+        private System.Windows.Forms.Label lblCharContaining;
         private System.Windows.Forms.Label lblLowerCase;
         private System.Windows.Forms.Label lblUperCase;
         private System.Windows.Forms.Label lblNumbers;
         private System.Windows.Forms.Label lblSymbols;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label lblOverlay;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblStrength;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
 
